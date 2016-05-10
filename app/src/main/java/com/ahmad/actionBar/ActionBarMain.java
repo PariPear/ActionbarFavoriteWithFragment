061ac30b2a3c10514742071b749873591bb17607
PariPear <pare_saai@hotmail.com>
2016-05-10 20:41:19 +0700

    v.3

--- app/src/main/java/com/ahmad/actionBar/ActionBarMain.java
+++ app/src/main/java/com/ahmad/actionBar/ActionBarMain.java
@@ -7,17 +7,71 @@ import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.io.File;
 
 @SuppressLint("NewApi")
 public class ActionBarMain extends Activity implements TabListener {
 	RelativeLayout rl;
 
+
+	private ViewPager viewPager;
+
+	private String[] FilePathStrings;
+	private String[] FileNameStrings;
+	private File[] listFile;
+	GridView grid;
+	GridViewAdapter adapter;
+	File file;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_action_bar_main);
+
+		// Check for SD Card
+		if (!Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED)) {
+			Toast.makeText(this, "Error! No SDCARD Found!", Toast.LENGTH_LONG)
+					.show();
+		} else {
+			// Locate the image folder in your SD Card
+			file = new File(Environment.getExternalStorageDirectory()
+					+ File.separator + "Collections");
+			// Create a new folder if no folder named SDImageTutorial exist
+			file.mkdirs();
+		}
+		if (file.isDirectory()) {
+			listFile = file.listFiles();
+			// Create a String array for FilePathStrings
+			FilePathStrings = new String[listFile.length];
+			// Create a String array for FileNameStrings
+			FileNameStrings = new String[listFile.length];
+
+			for (int i = 0; i < listFile.length; i++) {
+				// Get the path of the image file
+				FilePathStrings[i] = listFile[i].getAbsolutePath();
+				// Get the name image file
+				FileNameStrings[i] = listFile[i].getName();
+			}
+		}
+
+		// Locate the GridView in gridview_main.xml
+		grid = (GridView) findViewById(R.id.mainGrid);
+		// Pass String arrays to LazyAdapter Class
+		adapter = new GridViewAdapter(this, FilePathStrings, FileNameStrings);
+		// Set the LazyAdapter to the GridView
+		grid.setAdapter(adapter);
+		beginActionMode(); // this might be used by a long press or by button tam in action bar...
+
+
 		try {
 			rl = (RelativeLayout) findViewById(R.id.mainLayout);
 			fragMentTra = getFragmentManager().beginTransaction();
@@ -36,10 +90,15 @@ public class ActionBarMain extends Activity implements TabListener {
 		} catch (Exception e) {
 			e.getMessage();
 		}
+	}
+
+	public void beginActionMode(){
+		grid.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+	}
 		/**
 		 * Hiding Action Bar
 		 */
-	}
+
 
 	FragMent1 fram1;
 	FragmentTransaction fragMentTra = null;
